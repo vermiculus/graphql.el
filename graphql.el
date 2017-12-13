57a3dfe5bf384696eb9e902643a79b7b010b988e
Sean Allred <code@seanallred.com>
2017-12-12 22:19:30 -0600

    Correct capitalization

--- graphql.el
+++ graphql.el
@@ -22,7 +22,7 @@
 
 ;;; Commentary:
 
-;; Graphql.el provides a generally-applicable domain-specific language
+;; GraphQL.el provides a generally-applicable domain-specific language
 ;; for creating and executing GraphQL queries against your favorite
 ;; web services.
 
